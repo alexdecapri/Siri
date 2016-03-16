4d8b993b4add534b22f4b753129193c53f5eae33
alexdecapri <alexdecapri@gmail.com>
2016-03-15 19:56:47 -0600

    everything done, postman working, but not sure how to connect to siri client on devmountain.github.io

--- server.js
+++ server.js
@@ -15,3 +15,29 @@ app.get("/", function(req, res) {
     message: getRandomMessage
   }))
 });
+
+app.options('/', function(req, res) {
+  res.status(200).set({
+    'Content-Type': 'application/json',
+    'Access-Control-Allow-Origin': '*',
+    'Access-Control-Allow-Methods': 'OPTIONS, GET, POST',
+    'Access-Control-Allow-Headers': 'Origin, X-Requested-With, Content-Type, Accept',
+    'X-XSS-Protection': '1; mode=block',
+    'X-Frame-Options': 'SAMEORIGIN',
+    'Content-Security-Policy': "default-src 'self' devmountain.github.io"
+  }).send();
+});
+
+app.get('/', function(req, res) {
+  res.status(200).set({
+    'Content-Type': 'application/json',
+    'Access-Control-Allow-Origin': '*',
+    'Access-Control-Allow-Methods': 'OPTIONS, GET, POST',
+    'Access-Control-Allow-Headers': 'Origin, X-Requested-With, Content-Type, Accept',
+    'X-XSS-Protection': '1; mode=block',
+    'X-Frame-Options': 'SAMEORIGIN',
+    'Content-Security-Policy': "default-src 'self' devmountain.github.io"
+  }).send(JSON.stringify({
+    message: getRandomMessage
+  }));
+});
