bf17afb8ffdb81f60d6239a491749310b9f766f9
alexdecapri <alexdecapri@gmail.com>
2016-03-15 19:46:36 -0600

    returning an object, randomizer is working

--- server.js
+++ server.js
@@ -6,10 +6,12 @@ app.listen(8887, function() {
   console.log("Listening on port 8887");
 });
 
-var messages = ["Hello there.", "I'm sorry, I cannot take any requests at this time.", "I cna tell you how to do that!"];
+var messages = ["Hello there.", "I'm sorry, I cannot take any requests at this time.", "I can tell you how to do that!"];
+
+var getRandomMessage = messages[Math.floor(Math.random()*messages.length)];
 
 app.get("/", function(req, res) {
   res.send(JSON.stringify({
-    message: getRandomMessage()
+    message: getRandomMessage
   }))
 });
